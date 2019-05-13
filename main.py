a3c73b5a16e0022a6cf3e5e553b87f97dcd6a48c
DuncanCopeland <tug04599@temple.edu>
2019-05-13 11:49:28 -0400

    test work on webscraping.

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+#Test Work on web scraping
 def main():
-    print("First!")
+    url = 'http://morse-brode.herokuapp.com/'
+    response = requests.get(url)
+    html = response.content
+    soup = BeautifulSoup(html, 'html.parser')
+    print(soup.prettify())
 
 
 if __name__ == "__main__":
