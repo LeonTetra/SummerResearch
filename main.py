7c322ec7bedfb4c38c45819d669f526dce3e7100
DuncanCopeland <tug04599@temple.edu>
2019-05-28 14:39:36 -0400

    switching to selenium

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import webscraper as web
 
 def main():
     ws = web.WebScraper()
-    res = ws.get_search(topic='Germany', query='weimar')
-    print(res[0])
-    ws.get_article(res[0])
+
 
 if __name__ == "__main__":
     main()
--- webscraper.py
+++ webscraper.py
@@ -1,51 +1,58 @@
 import requests
 from bs4 import BeautifulSoup
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 class WebScraper:
     # url to search site keywords are applied to the url such that the
     # webscraper will only target those results
     # results are stored in a list of JSON
     def __init__(self):
-        self.lib_link = 'https://librarysearch.temple.edu'
-
-    def get_search(self, topic, query):
-        search_url = self.lib_link + '/articles?f[lang][]=eng&f[rtype][]=articles&f[tlevel][]=peer_reviewed&f[tlevel][]=online_resources&f[topic][]=' \
-                     + topic + '&per_page=100&q=' + query
-        response = requests.get(search_url)
-        html = response.content
-        soup = BeautifulSoup(html, 'html.parser')
-        hits = soup.find_all('h3', {'class': 'index_title document-title-heading col-sm-9 col-lg-10'})
-        results = []
-        for i in hits:
-            num = i.text[10]
-            title = i.text[21:(len(i.text)-1)]
-            for j in i.children:
-                if j.name == 'span' or j == '\n':
-                    del j
-                else:
-                    link = j['href']
-
-            results.append(Result(title, num, link))
-        return results
-
-    def get_article(self, result):
-        response = requests.get(self.lib_link + result.link)
-        html = response.content
-        soup = BeautifulSoup(html, 'html.parser')
-        box = soup.find('iframe')
-        ifr_link = box['src']
-        ifr = requests.get(ifr_link)
-        soup = BeautifulSoup(ifr.content, 'html.parser')
-        print(soup.prettify())
-        print()
-
-
-class Result:
-    def __init__(self, title, num, link):
-        self.title = title
-        self. num = num
-        self.link = link
-
-    def __str__(self):
-        return "Number: " + self.num + "| Title: " + self.title + "| Link: " + self.link
+        driver = webdriver.Firefox()
+        driver.get("https://librarysearch.temple.edu")
+#     def __init__(self):
+#         self.lib_link = 'https://librarysearch.temple.edu'
+#
+#     def get_search(self, topic, query):
+#         search_url = self.lib_link + '/articles?f[lang][]=eng&f[rtype][]=articles&f[tlevel][]=peer_reviewed&f[tlevel][]=online_resources&f[topic][]=' \
+#                      + topic + '&per_page=100&q=' + query
+#         response = requests.get(search_url)
+#         html = response.content
+#         soup = BeautifulSoup(html, 'html.parser')
+#         hits = soup.find_all('h3', {'class': 'index_title document-title-heading col-sm-9 col-lg-10'})
+#         results = []
+#         for i in hits:
+#             num = i.text[10]
+#             title = i.text[21:(len(i.text)-1)]
+#             for j in i.children:
+#                 if j.name == 'span' or j == '\n':
+#                     del j
+#                 else:
+#                     link = j['href']
+#
+#             results.append(Result(title, num, link))
+#         return results
+#
+#     def get_article(self, result):
+#         response = requests.get(self.lib_link + result.link)
+#         html = response.content
+#         soup = BeautifulSoup(html, 'html.parser')
+#         box = soup.find('iframe')
+#         ifr_link = box['src']
+#         ifr = requests.get(ifr_link)
+#         soup = BeautifulSoup(ifr.content, 'html.parser')
+#         print(soup.prettify())
+#         print()
+#
+#
+# class Result:
+#     def __init__(self, title, num, link):
+#         self.title = title
+#         self.num = num
+#         self.link = link
+#
+#     def __str__(self):
+#         return "Number: " + self.num + "| Title: " + self.title + "| Link: " + self.link
