ff8bfded992afb2f9cd89edff0e1ced54853d512
DuncanCopeland <tug04599@temple.edu>
2019-05-30 11:20:03 -0400

    Started Differentiating different databases and journals. Will need to automate for each one, since some contain full text then and there, while some demand download.

--- webscraper.py
+++ webscraper.py
@@ -33,6 +33,50 @@ class WebScraper:
             access[1] = (f.readline()).strip('\n')
         return access
 
+    def __identify_db(self):
+        # list dbs to draw articles from. Identified by 5 character code
+        # e.g. JSTOR = JSTOR and SPRNG = Springer
+        dbs = ['JSTOR', 'EBSCO', 'SPRNG', 'SCIDI', 'KARGR', 'JAMAN', 'JNNPB', 'PROQS', 'WOLSK', 'JSAGE', 'OVIDS', 'ACADO', 'DUKEU', 'TANDF', 'GALEG']
+        wait = WebDriverWait(self.driver, 20)
+        try:
+            ident = wait.until(browser_has_url_element('.libproxy'))
+        except TimeoutException:
+            print('UNKNOWN')
+            return 'UNKNOWN'  #unknown
+        print(ident)
+        if ident == 'www-jstor-org':
+            return dbs[0]
+        elif ident == 'eb.a.ebscohost.com' or ident == 'eb.b.ebscohost.com' or ident == 'openurl-ebscohost-com':
+            return dbs[1]
+        elif ident == 'link-springer-com':
+            return dbs[2]
+        elif ident == 'www-sciencedirect-com':
+            return dbs[3]
+        elif ident == 'www-karger-com':
+            return dbs[4]
+        elif ident == 'jamanetwork-com':
+            return dbs[5]
+        elif ident == 'jnnp-bmj-com':
+            return dbs[6]
+        elif ident == 'search-proquest-com':
+            return dbs[7]
+        elif ident == 'login-wolterskluwer-com':
+            return dbs[8]
+        elif ident == 'journals-sagepub-com':
+            return dbs[9]
+        elif ident == 'ovidsp-tx-ovid-com':
+            return dbs[10]
+        elif ident == 'academic-oup-com':
+            return dbs[11]
+        elif ident == 'read-dukeupress-edu':
+            return dbs[12]
+        elif ident == 'www-tandfonline-com':
+            return dbs[13]
+        elif ident == 'o.galegroup.com' or ident == 'i.galegroup.com':
+            return dbs[14]
+        else:
+            return 'X : DATABASE OR JOURNAL NOT LISTED'
+
     def search(self):
         btns_onl = self.driver.find_elements_by_id("online_button")
         for i in btns_onl:
@@ -47,11 +91,19 @@ class WebScraper:
                 lnks[2].send_keys(Keys.RETURN)
                 while len(self.driver.window_handles) < 2:
                     continue
-            self.driver.switch_to.window(self.driver.window_handles[-1])
+            self.driver.switch_to.window(self.driver.window_handles[1])
             if not self.logged_in:
                 self.logged_in = self.login()
+
+            self.parse()
+
             self.driver.switch_to.window(self.whandle)
             self.driver.switch_to.default_content()
+    def parse(self):
+        cur_db = self.__identify_db()
+        if cur_db == 'UNKNOWN' or cur_db[0] == 'X':
+            self.driver.close()
+
 
     def load(self, url):
         self.driver.get(url)
@@ -73,3 +125,35 @@ class WebScraper:
         self.driver.find_element_by_tag_name('button').click()
         return True
 
+
+class browser_has_url_element:
+    def __init__(self, expected):
+        self.expected = expected
+
+    def __call__(self, driver):
+        url = str(driver.current_url)
+        try:
+            s: int = url.index(self.expected)
+            return url[8:s]
+        except ValueError:
+            return False
+
+
+
+# class element_has_css_class(object):
+#     """An expectation for checking that an element has a particular css class.
+#
+#     locator - used to find the element
+#     returns the WebElement once it has the particular css class
+#     """
+#
+#     def __init__(self, locator, css_class):
+#         self.locator = locator
+#         self.css_class = css_class
+#
+#     def __call__(self, driver):
+#         element = driver.find_element(*self.locator)  # Finding the referenced element
+#         if self.css_class in element.get_attribute("class"):
+#             return element
+#         else:
+#             return False
