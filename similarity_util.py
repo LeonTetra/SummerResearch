16ebcf51dbf58c1c2d0e560794f139eb54150e75
DuncanCopeland <tug04599@temple.edu>
2019-06-11 12:55:40 -0400

    Continued Work on BagOFWords. Think I got it working.

--- similarity_util.py
+++ similarity_util.py
@@ -14,7 +14,26 @@ class BagOfWords:
         self.word_bag = self.__create_word_bag(words)
 
     def __create_word_bag(self, words):
-        split = str(words).split(' ')
-        count = 0
-        bag = [];
-        bag.append(split)
+        split = words.split(' ')
+        length = len(split)
+        wb = []
+        wb.append(words)
+        segs = 1
+        step = length
+        while step >= 1:
+            for i in range(0, segs):
+                wb.append(self.__create_long_string(split[(i*step):((i*step)+step)]))
+            step = step // 2
+            segs *= 2
+        return wb
+
+    def __recursively_create_word_list(self, words, length):
+        if length <= 1:
+            return ""
+
+    @staticmethod
+    def __create_long_string(words):
+        s = ""
+        for i in words:
+            s += i + " "
+        return s[0:len(s)-1]
