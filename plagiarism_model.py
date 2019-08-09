ae600b7bc4d0ee7a040cc463411c95006d4106f0
DuncanCopeland <tug04599@temple.edu>
2019-08-09 12:51:48 -0400

    Final commit

--- plagiarism_model.py
+++ plagiarism_model.py
@@ -1,5 +1,6 @@
 import pandas
 from matplotlib import pyplot as plt
+import sklearn as skl
 
 class MLPlagiarismChecker:
     def __init__(self, data, submission, sim_indices):
@@ -7,10 +8,25 @@ class MLPlagiarismChecker:
         self.submission = submission
         self.sim_indices = sim_indices
         dfs = self.build_dataframes()
-        #build model and plot figures
+        km = skl.cluster.KMeans()
+        X = (dfs['Similarity Index']).values.reshape(-1, 1)
+        model = km.fit(X)
+        cluster_labels = model.predict(X)
+        cent = model.cluster_centers_
+        self.plot(model, cluster_labels, cent, dfs)
 
     def build_dataframes(self):
         print()
         df = pandas.DataFrame(self.data)
         df['Similarity Index'] = pandas.Series(self.sim_indices)
         return df
+
+    def plot(self, model, cluster_labels, cent, df):
+        kmeans = pandas.DataFrame(cluster_labels)
+        for i in df:
+            print()
+            for j in df:
+                scatter = plt.scatter(df[i], df[j], c=kmeans[0])
+                plt.colorbar(scatter)
+                plt.savefig(i + '_' + j + '.png')
+                plt.clf()
